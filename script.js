f7480fac8f4e86bed4edf2ba19797ffa67290e92
Kabir <builtbykabir@proton.me>
2026-01-12 23:44:00 +0530

    add search and filter

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
 const url =
   "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca3";
+
 const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector(".search-input");
+const regionSelect = document.querySelector(".region-select");
+
+let allCountries = [];
 
 fetch(url)
   .then((response) => response.json())
   .then((countryData) => {
-    countryData.forEach((country) => {
-      const countryCard = document.createElement("div");
-      countryCard.classList.add("card");
-      countryCard.innerHTML = `
-                    <img src="${country?.flags?.svg}" alt="">
-                    <div class="text-content">
-                    <h2>${country?.name?.common}</h2>
-                    <div class="details">
-                    <p>Population:&nbsp;<span>${country?.population}</span></p>
-                    <p>Region:&nbsp;<span>${country?.region}</span></p>
-                    <p>Capital:&nbsp;<span>${country?.capital[0]}</span></p>
-                    </div>
-                    </div>
-                    `;
-      countryCard.addEventListener("click", () => {
-        window.location.href = `country.html?code=${country.cca3}`;
-      });
-      cardContainer.append(countryCard);
-    });
+    allCountries = countryData;
+    renderCountries(allCountries);
   });
 
-// {
-//     "flags": {
-//         "png": "https://flagcdn.com/w320/ag.png",
-//         "svg": "https://flagcdn.com/ag.svg",
-//         "alt": "The flag of Antigua and Barbuda has a red field with an inverted isosceles triangle based on the top edge and spanning the height of the field. This triangle has three horizontal bands of black, light blue and white, with the light blue band half the height of the two other bands. The top half of a golden-yellow sun is situated in the lower two-third of the black band to depict a rising sun."
-//     },
-//     "name": {
-//         "common": "Antigua and Barbuda",
-//         "official": "Antigua and Barbuda",
-//         "nativeName": {
-//             "eng": {
-//                 "official": "Antigua and Barbuda",
-//                 "common": "Antigua and Barbuda"
-//             }
-//         }
-//     },
-//     "cca3": "ATG",
-//     "capital": [
-//         "Saint John's"
-//     ],
-//     "region": "Americas",
-//     "population": 103603
-// }
+function renderCountries(countries) {
+  cardContainer.innerHTML = "";
+
+  if (!countries.length) {
+    cardContainer.innerHTML = "<p>No countries found.</p>";
+    return;
+  }
+
+  countries.forEach((country) => {
+    const countryCard = document.createElement("div");
+    countryCard.classList.add("card");
+
+    countryCard.innerHTML = `
+      <img src="${country.flags?.svg}" alt="">
+      <div class="text-content">
+        <h2>${country.name?.common}</h2>
+        <div class="details">
+          <p>Population:&nbsp;<span>${country.population.toLocaleString()}</span></p>
+          <p>Region:&nbsp;<span>${country.region}</span></p>
+          <p>Capital:&nbsp;<span>${country.capital?.[0] ?? "N/A"}</span></p>
+        </div>
+      </div>
+    `;
+
+    countryCard.addEventListener("click", () => {
+      window.location.href = `country.html?code=${country.cca3}`;
+    });
+
+    cardContainer.append(countryCard);
+  });
+}
+
+function applyFilters() {
+  const searchQuery = searchInput.value.toLowerCase().trim();
+  const selectedRegion = regionSelect.value;
+
+  let filteredCountries = allCountries;
+
+  if (selectedRegion !== "All") {
+    filteredCountries = filteredCountries.filter(
+      (country) => country.region === selectedRegion
+    );
+  }
+
+  if (searchQuery) {
+    filteredCountries = filteredCountries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchQuery)
+    );
+  }
+
+  renderCountries(filteredCountries);
+}
+
+searchInput.addEventListener("input", applyFilters);
+regionSelect.addEventListener("change", applyFilters);
+
